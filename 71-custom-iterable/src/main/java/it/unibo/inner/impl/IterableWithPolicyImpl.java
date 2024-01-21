fc6eb245acde66e73a000c8e75d6f42be3a4a4ea
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-21 18:32:28 +0100

    Esercizio 71: Completata implementazione di IterableWithPolicyImpl.java

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,7 +1,46 @@
 package it.unibo.inner.impl;
 
-import it.unibo.inner.api.IterableWithPolicy;
+import java.util.Iterator;
+import it.unibo.inner.api.*;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
     
+    private final T arr[];
+
+    public IterableWithPolicyImpl(final T elem[]){
+        this.arr = elem;
+    }
+
+    public void setIterationPolicy(Predicate<T> filter){}
+
+    private class IteratorImpl<T> implements Iterator<T> {
+
+        private final T array[]; 
+        private int current;
+
+        public IteratorImpl(final T array[]){
+            if (array == null) {
+                throw new java.lang.IllegalArgumentException();
+            }
+            this.array = array;
+            this.current = 0;
+        }
+
+        public boolean hasNext(){
+            return this.current == (this.array.length - 1);
+        }
+
+        public T next(){
+            if (this.hasNext()) {
+                return this.array[++this.current];
+            } else {
+                throw new java.util.NoSuchElementException();
+            }
+        }        
+    }
+
+    public Iterator<T> iterator(){
+        return new IteratorImpl<>(arr);
+    }
+
 }
