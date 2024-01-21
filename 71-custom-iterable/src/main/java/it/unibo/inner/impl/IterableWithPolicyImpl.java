52567d1c00c17a06b87c312a7605ebf2de13854c
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-21 19:36:11 +0100

    Esercizio 71: Aggiunto campo predicateBehavior e un costruttore a due argomenti in IterableWithPolicyImpl.java. Implementazione costruttore con singolo argomento non completata

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -5,21 +5,28 @@ import java.util.Iterator;
 import it.unibo.inner.api.*;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
-    
-    private final T[] arr;
 
-    public IterableWithPolicyImpl(final T[] elem){
+    private final T[] arr;
+    private Predicate<T> predicateBehavior;
+
+    public IterableWithPolicyImpl(final T[] elem, final Predicate<T> predicate) {
         this.arr = elem;
+        this.predicateBehavior = predicate;
     }
 
-    public void setIterationPolicy(Predicate<T> filter){}
+    public IterableWithPolicyImpl(final T[] elem) {
+        this(elem, null);
+    }
+
+    public void setIterationPolicy(Predicate<T> filter) {
+    }
 
     private class IteratorImpl<T> implements Iterator<T> {
 
-        private final T[] array; 
+        private final T[] array;
         private int current;
 
-        public IteratorImpl(final T[] array){
+        public IteratorImpl(final T[] array) {
             if (array == null) {
                 throw new java.lang.IllegalArgumentException();
             }
@@ -27,24 +34,24 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
             this.current = 0;
         }
 
-        public boolean hasNext(){
+        public boolean hasNext() {
             return this.current < (this.array.length);
         }
 
-        public T next(){
+        public T next() {
             if (this.hasNext()) {
                 return this.array[this.current++];
             } else {
                 throw new java.util.NoSuchElementException();
             }
-        }        
+        }
     }
 
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new IteratorImpl<>(arr);
     }
 
-    public String toString(){
+    public String toString() {
         return Arrays.deepToString(arr);
     }
 }
