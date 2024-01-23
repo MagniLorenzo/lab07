8084f275379fbe41e91f5d02dd5c3c4eb1b22c4a
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-23 17:44:18 +0100

    Esercizio 72: Creazione enum Month in MonthSortedNested.java con prima implementazione

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,5 +1,6 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
@@ -8,6 +9,55 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+
+    private static final int SHORT_MONTH = 28;
+    private static final int USUAL_MONTH = 30;
+    private static final int LONG_MONTH = 31;
+
+    public static enum Month {
+        JANUARY(LONG_MONTH),
+        FEBRUARY(SHORT_MONTH),
+        MARCH(LONG_MONTH),
+        APRIL(USUAL_MONTH),
+        MAY(LONG_MONTH),
+        JUNE(USUAL_MONTH),
+        JULY(LONG_MONTH),
+        AUGUST(LONG_MONTH),
+        SEPTEMBER(USUAL_MONTH),
+        OCTOBER(LONG_MONTH),
+        NOVEMBER(USUAL_MONTH),
+        DECEMBER(LONG_MONTH);
+
+        private final int daysNumber;
+
+        private Month(final int days) {
+            this.daysNumber = days;
+        }
+
+        public int getDaysNumber() {
+            return this.daysNumber;
+        }
+
+        public Month fromString(final String str) {
+            final ArrayList<Month> result = new ArrayList<>();
+            final String upperStr = str.toUpperCase();
+            for (final Month mese : Month.values()) {
+                final String monthName = mese.name();
+                if (monthName.contains(upperStr)) {
+                    result.add(mese);
+                }
+            }
+            switch (result.size()) {
+                case 0:
+                    throw new IllegalArgumentException("No month with such name");
+                case 1:
+                    return result.get(0);
+                default:
+                    throw new IllegalArgumentException("Ambiguos entry" + result.toString());
+            }
+
+        }
+    }
 
     @Override
     public Comparator<String> sortByDays() {
